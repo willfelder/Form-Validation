55662da52ea5fc02c7742ec406cd3e174c030c72
William Lengenfelder <willfelder@users.noreply.github.com>
2021-01-12 07:54:29 -0300

    updating

---
+++ script.js
@@ -0,0 +1,48 @@
+const formBox = document.getElementById('formbox');
+const usernameBox = document.getElementById('username');
+const emailBox = document.getElementById('email');
+const passwordBox = document.getElementById('password');
+const passordCheckBox = document.getElementById('passwordTwo');
+
+formBox.addEventListener('submit', (e) => {
+    e.preventDefault();
+        checkInputs();
+});
+
+   function checkInputs(){
+       const usernameBoxValue = usernameBox.value.trim();
+       const emailBoxValue = emailBox.value.trim();
+       const passwordBoxValue = passwordBox.value.trim();
+       const passordCheckBoxValue = passordCheckBox.value.trim();
+
+          if(usernameBoxValue === ''){
+              setErrorFor(usernameBox, 'Username can not be blank');
+          } else {
+              setSucessFor(usernameBox);
+          }
+
+          if(emailBoxValue === ''){
+              setErrorFor(emailBox, 'Email can not be blank');
+          } else if(!isEmail(emailBox)) {
+              setErrorFor(emailBox, 'Email is not valid')
+          } else {
+              setSucessFor(emailBox);
+          }
+   }
+
+    function setErrorFor(input, message){
+        const formControlBox = input.parentElement;
+        const smallBox = formControlBox.querySelector('small');
+
+        smallBox.innerText = message;
+        formControlBox.className = 'form-control-area error';
+    }
+
+    function setSucessFor(input){
+        const formControlBox = input.parentElement;
+        formControlBox.className = 'form-control-area sucess';
+    }
+
+    function isEmail(emailBox){
+        return /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/test(emailBox);
+    }
